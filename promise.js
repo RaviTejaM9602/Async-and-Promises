748d4b46a542cca2b3722015063e8f325a33f2dc
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-01-14 21:49:27 +0000

    Testing with Fake Request

--- promise.js
+++ promise.js
@@ -1,14 +1,59 @@
-const willGetYouDog = new Promise((resolve,reject) => {
-  const rand = Math.random();
-  if (rand > 0.5 ){
-    resolve();
-  }else{
-    reject();
-  }
-});
-willGetYouADog.then(() => {
-	console.log('YAY WE GOT A DOG!!!!');
-});
-willGetYouADog.catch(() => {
-	console.log(':( NO DOG');
-});
+// const willGetYouDog = new Promise((resolve,reject) => {
+//   const rand = Math.random();
+//   if (rand > 0.5 ){
+//     resolve();
+//   }else{
+//     reject();
+//   }
+// });
+// willGetYouADog.then(() => {
+// 	console.log('YAY WE GOT A DOG!!!!');
+// });
+// willGetYouADog.catch(() => {
+// 	console.log(':( NO DOG');
+// });
+
+//This is a FAKE Http Request Function
+//It takes 1 second to resolve or reject the promise, depending on the url that is passed in
+const fakeRequest = (url) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const pages = {
+				'/users' : [
+					{ id: 1, username: 'Bilbo' },
+					{ id: 5, username: 'Esmerelda' }
+				],
+				'/about' : 'This is the about page!'
+			};
+			const data = pages[url];
+			if (data) {
+				resolve({ status: 200, data }); //resolve with a value!
+			}
+			else {
+				reject({ status: 404 }); //reject with a value!
+			}
+		}, 1000);
+	});
+};
+
+fakeRequest('/users')
+	.then((res) => {
+		console.log('Status Code', res.status);
+		console.log('Data', res.data);
+		console.log('REQUEST WORKED!');
+	})
+	.catch((res) => {
+		console.log(res.status);
+		console.log('REQUEST FAILED');
+	});
+
+fakeRequest('/dogs')
+	.then((res) => {
+		console.log('Status Code', res.status);
+		console.log('Data', res.data);
+		console.log('REQUEST WORKED!');
+	})
+	.catch((res) => {
+		console.log(res.status);
+		console.log('REQUEST FAILED');
+	});
