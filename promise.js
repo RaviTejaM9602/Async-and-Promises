4d79758f9cd14c499ad54cdafcfc9565d767f51b
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-01-14 21:31:19 +0000

    Started with Promise Syntax

---
+++ promise.js
@@ -0,0 +1,14 @@
+const willGetYouDog = new Promise((resolve,reject) => {
+  const rand = Math.random();
+  if (rand > 0.5 ){
+    resolve();
+  }else{
+    reject();
+  }
+});
+willGetYouADog.then(() => {
+	console.log('YAY WE GOT A DOG!!!!');
+});
+willGetYouADog.catch(() => {
+	console.log(':( NO DOG');
+});
